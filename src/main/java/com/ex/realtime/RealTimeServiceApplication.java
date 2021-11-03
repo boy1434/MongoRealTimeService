37408da77c219f609fea3a67fabd6ac18926b983
Your Name <you@example.com>
2021-11-03 14:20:26 +0900

    [RealTimeService] 1. 환경설정 및 패키지 생성 완료

---
+++ src/main/java/com/ex/realtime/RealTimeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ex.realtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+public class RealTimeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeServiceApplication.class, args);
+	}
+
+}
